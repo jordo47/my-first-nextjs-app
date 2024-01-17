460e6137413b532ffa8e0e14dff4440969d6ec50
jordo47 <jordojordo47@gmail.com>
2024-01-17 12:38:43 -0600

    fix eslint

--- nextjs-dashboard/app/page.tsx
+++ nextjs-dashboard/app/page.tsx
@@ -33,15 +33,15 @@ export default function Page() {
             src="/hero-desktop.png"
             width={1000}
             height={760}
-            className='hidden md:block'
-            alt='Screenshots of the dashboard project showing desktop version'
+            className="hidden md:block"
+            alt="Screenshots of the dashboard project showing desktop version"
           />
           <Image
             src="/hero-mobile.png"
             width={560}
             height={620}
-            className='block md:hidden'
-            alt='Screenshots of the dashboard project showing mobile version'
+            className="block md:hidden"
+            alt="Screenshots of the dashboard project showing mobile version"
           />
         </div>
       </div>
